d6b2a3daccb92640df9274063a99f05f66640350
linux-kbl <edgardo.torres@hotmail.com>
2021-02-01 13:18:17 -0600

    add helper

--- src/components/ControlGasto.js
+++ src/components/ControlGasto.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {revisarPresupuesto} from '../helpers';
 
 const ControlGasto = ({presupuesto, resto}) => {
   return(
     <>
       <div>
         <div>Presupuesto: ${presupuesto}</div>
-        <div>Resto: ${resto}</div>
+        <div class={revisarPresupuesto(presupuesto, resto)}>Resto: ${resto}</div>
       </div>
     </>
 
